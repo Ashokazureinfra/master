ecc1ae839ef85568b4fb365cb702982d9b80a083
Ashokazureinfra <166581602+Ashokazureinfra@users.noreply.github.com>
2024-04-10 00:11:01 -0500

    Add files via upload

---
+++ rg1.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm"{
+    resource_group_name = "rg123"
+    storage_account_name = "testtestdemo123"
+    container_name = "terraform"
+    key = "rg.tfstate"
+  }
+
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+      features {}
+}
+
+
+resource "azurerm_resource_group" "example1234" {
+  name     = "exampleRG12"
+  location = "East US"
+}
+output "rg"{
+  value = azurerm_resource_group.example1234.id
+  description = "output value"
+
+}
